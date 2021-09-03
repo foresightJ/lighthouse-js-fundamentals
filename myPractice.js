f7290249894bdc4e1a1aaa95de8d4a4f049c8abd
Foresight John <foresightjohn@Foresights-MacBook-Pro.local>
2021-09-03 15:44:00 -0400

    personal practice on js

---
+++ myPractice.js
@@ -0,0 +1,12 @@
+
+for (let i = 100; i <= 200; i++) {
+ if (i % 3 == 0) {
+   console.log('Loopy')
+  }
+   else if (i % 4 == 0) {
+    console.log('Lighthouse')
+  }
+   else if (1 % 12 == 0){
+     console.log('LoopyLighthouse')
+   }
+}
